09a74394a8fa1d004b07576e1d11e856a546efb5
natalia646 <nataxash46@gmail.com>
2022-08-29 14:31:53 +0300

    freeCC

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ function calcPowerNumber(a, n){// рахує n степіль від числа 
   }
   return recolt
 }
-console.log(calcPowerNumber(12, 0))
+// console.log(calcPowerNumber(12, 0))
 
 
 function calcPowerRecurs (a, n){
@@ -35,4 +35,62 @@ function calcPowerRecurs (a, n){
     return a * calcPowerRecurs(a, n-1)
    }
 }
-console.log(calcPowerRecurs(3, 3))
+// console.log(calcPowerRecurs(3, 3))
+
+
+
+
+function calcMultiplArr (arr, n){
+  let resolt = 1
+  for(let i = 0; i < n; i++ ){
+    resolt *= arr[i]
+  }
+  return resolt
+}
+ console.log(calcMultiplArr([2, 3, 4, 4, 6], 2 ))
+
+ function calcArrRecurs(arr, n){
+  if(n === 1){
+    return arr[0]
+  }else{
+    return arr[n-1] * calcArrRecurs(arr, n-1)
+  }  
+ }
+ console.log(calcArrRecurs([2, 3, 4, 4, 6], 1 ))
+
+
+
+ function calcSumm(arr, n){
+  let resolt = 0
+  for(let i = 0; i < n; i++){
+     resolt += arr[i]
+  }
+  return resolt
+ }
+ console.log(calcSumm([2, 3, 4, 4, 6], 3 ))
+
+
+
+
+ function calcSummRecurs(arr, n){
+if(n === 0){
+  return 0
+}else{
+  return arr[n-1] + calcSummRecurs(arr, n-1)
+}
+ }
+ console.log(calcSummRecurs([1], 1 ))
+
+
+
+ function rangeOfNumbers(startNum, endNum) {
+  if (startNum === endNum) {
+    return [startNum];
+  } else {
+    const countArray = rangeOfNumbers(startNum, endNum - 1);
+    countArray.push(endNum);
+    return countArray;
+  }
+}
+
+console.log(rangeOfNumbers(5, 9))
