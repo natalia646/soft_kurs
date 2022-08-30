d5e506bee0173d0c0a570dba053c46809ca763ec
natalia646 <75090544+natalia646@users.noreply.github.com>
2022-08-30 11:22:25 +0300

    Update my_code.js

--- js/my_code.js
+++ js/my_code.js
@@ -25,3 +25,36 @@ function randomRange(myMin, myMax) {
 }
 console.log(randomRange(4, 18));
 //------------------------------------------------------------
+
+// використання collback функції
+const enterNum = prompt("Enter the numbers and the action separated by commas").split(",");
+ const action = enterNum[2];
+ const a = +enterNum[0];
+ const b = +enterNum[1];
+
+function checkAction(a, b, action, plus, minus, multiply, divide){
+    action == "+" ? plus():
+    action == "-" ? minus():
+    action == "*" ? multiply():
+    action == "/" ? divide():
+    null
+}
+
+function calcPlus(){
+    let sum =  a + b;
+    alert(sum)
+}
+function calcMinus(){
+    let difference = a - b;
+    alert(difference)
+}
+function calcMultiply(){
+   let product = a * b;
+   alert(product)
+}
+function calcDivide(){
+    let fraction =  a / b;
+    alert(fraction)
+}
+
+checkAction(a, b, action, calcPlus, calcMinus, calcMultiply, calcDivide);
