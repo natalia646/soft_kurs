36919c5cfacd6fbe4a6cd1fb242fbc064738a5f4
natalia646 <nataxash46@gmail.com>
2022-10-04 11:12:26 +0300

    new

--- js/index.js
+++ js/index.js
@@ -8,10 +8,9 @@
 
 // console.log(arr);
 
-
 // function oddCount(n){
 //   return Math.floor(n / 2)
-  
+
 // }
 // console.log(oddCount(7))
 
@@ -20,13 +19,55 @@
 // const a = [0, 1, 2, 3, 4, 5, 6, 7];
 // const res = a.replace(/[0-4]/g, ())
 
+// const myObj = {
+//   id: 1,
+//   firstName: "Lisa",
+//   age: 23,
+//   skils: ["js", "html"],
+//   wife: {
+//     nam: "ola",
+//     age: 24,
+//     skils: ["css", "java"],
+//   }
+// }
 
+// console.log(myObj.id)
+// console.log(myObj.skils[1])
+// console.log(myObj.wife.age)
+// console.log(myObj.wife.skils[0])
 
+// const users = [
+//   { skils: ["js", "css", ""], id: 1, name: "vasya", status: true, age: 31 },
+//   {
+//     skils: ["js", "css", "java"],
+//     id: 2,
+//     name: "petya",
+//     status: false,
+//     age: 25,
+//   },
+//   { skils: ["js", "css", "java"], id: 3, name: "anna", status: true, age: 32 },
+//   { skils: ["js", "css", ""], id: 4, name: "olya", status: false, age: 24 },
+// ];
 
+// for(let i = 0; i < users.length; i++){
+//   delete users[i].skils
+// }
 
+// console.log(users)
 
+// let maunth = 2;
 
-
-
-
-
+// switch (maunth) {
+//   case 1:
+//   case 2:
+//     console.log("You are Ko");
+//     break;
+//   case 3:
+//   case 4:
+//     console.log("You are Wot");
+//     break;
+//   case 5:
+//   case 6:
+//     console.log("You are Fishes");
+//     break;
+// }
