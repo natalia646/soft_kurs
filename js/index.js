a556e4ee8e1d92b50acb46dc2869463470e96230
natalia646 <nataxash46@gmail.com>
2022-10-04 11:12:17 +0300

    new

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// const arr = ["cat", "dog", "fish", 2, 3, 4, "table", "picture", "chair"];
+
+// //! splice
+// // arr.splice(3, 3);
+
+// arr.splice(3, 3, "arms", "legs");
+// arr.splice(3, 0, "men")
+
+// console.log(arr);
+
+
+// function oddCount(n){
+//   return Math.floor(n / 2)
+  
+// }
+// console.log(oddCount(7))
+
+// //! replace
+
+// const a = [0, 1, 2, 3, 4, 5, 6, 7];
+// const res = a.replace(/[0-4]/g, ())
+
+
+
+
+
+
+
+
+
+
+
--- js/my_code.js
+++ js/my_code.js
@@ -1,5 +1,4 @@
 //! Рукурсія
-
 function calcPowerNumber(a, n){   //цикл
     let recolt = 1
     for(let i = 0; i < n; i++){
@@ -18,7 +17,7 @@ function calcPowerNumber(a, n){   //цикл
  }
  console.log(calcPowerRecurs(3, 3))
 
- //факторіал рахує рекурсією
+ //!факторіал рахує рекурсією
 function calcFactorial(n){
   if(n === 1 ){
     return 1
@@ -40,14 +39,14 @@ function calc_Factorial(n){
 console.log(calc_Factorial(-3))
 
   //------------------------------------------------------------
-// рандомне число в певному діапазоні
+//! рандомне число в певному діапазоні
 function randomRange(myMin, myMax) {
   return   Math.floor(Math.random() * (myMax - myMin + 1)) + myMin ;
 }
 console.log(randomRange(4, 18));
 //------------------------------------------------------------
 
-// використання collback функції
+//! використання collback функції
 const enterNum = prompt("Enter the numbers and the action separated by commas").split(",");
  const action = enterNum[2];
  const a = +enterNum[0];
@@ -90,3 +89,22 @@ do{
 } while(isNaN(ageName))
 
 //------------------------------------------
+//!перетворення типів
+console.log(100 + ""); //number in string
+console.log(200 + 100 + "");
+console.log("" + 100 + 20);
+
+console.log(+"123");  //string in number
+console.log(parseInt("123djjd53"));
+console.log(parseFloat("145.45kskk"));
+
+
+console.log(!!-100); //number in boolean
+console.log(!!0);
+console.log(!!"shfj"); //string in boolean
+console.log(!!'');
+
+console.log(true + ""); // boolean in string
+
+console.log(+true); // boolean in number
+console.log(+0)
