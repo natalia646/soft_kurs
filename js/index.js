a89f9d89ffbb1031f083c430f3e9f4324f1cb6c3
natalia646 <nataxash46@gmail.com>
2022-12-01 17:09:46 +0200

    new

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 // import * as myModule from "./module.js";
 import calc from "./module.js";
-
 // console.log(myModule.calcFactorial(5))
 // console.log(myModule.randomRange(23, 34))
 // console.log(calc(2, 4, 5))
+
+
 
 const users = [
   {
@@ -24,5 +25,35 @@ const users = [
     nation: "UK",
     sex: "girl",
   },
+ 
 ];
 
+ const user = {
+    name: "vira",
+    age: 24,
+    nation: "Poland",
+    sex: "girl",
+  };
+
+ function getSwitch ({nation}){
+  switch(nation){
+    case "UK":
+      return 'Hello my British friend';
+    case 'Poland':
+      return 'Hello my best friend';
+    case 'Ukrain':
+      return  'Hello my love!';
+  }
+ }
+ console.log(getSwitch(users));
+
+
+ function calcSum (a, b){
+  return a + b
+ }
+
+ function audit (answer){
+   switch(true){
+
+   }
+ }
