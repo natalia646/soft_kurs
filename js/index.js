17de043d70c212b457b9edfd6d2fa8f16d0ff636
natalia646 <75090544+natalia646@users.noreply.github.com>
2022-12-08 13:54:26 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,1 @@
-// import * as myModule from "./module.js";
-// import calc from "./module.js";
-// console.log(myModule.calcFactorial(5))
-// console.log(myModule.randomRange(23, 34))
-// console.log(calc(2, 4, 5))
-
 
